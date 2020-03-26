824df9e4425114549576f4dedaf7eba72ece7610
Silas S. Brown <ssb22@cam.ac.uk>
2020-03-26 18:40:39 +0000

    Remove git-sync.sh (updates now done by homepage/Makefile itself)

--- git-sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull --no-edit
-wget -N http://ssb22.user.srcf.net/access/access.tar.gz
-tar -zxvf access.tar.gz
-tar -zxvf imgserver.tar.gz
-git add *.c++ *.h *.tbl COPYING *.dat *.sh *.htm
-git commit -am update && git push
